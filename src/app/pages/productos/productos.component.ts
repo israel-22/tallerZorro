43034adc0462203ce76e19218ba615e0b4865a0b
Cristhofer24 <cristhofer.elian.gramal@gmail.com>
2024-09-05 09:38:22 -0500

    Talwin

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
 export class ProductosComponent {
+  Create = false;
+  Read = false;
+  Update = false;
+  Delete = false;
 
+  openModalCreate() {
+    this.Create = true;
+
+  }
+  
+  openModalDelete() {
+  
+    this.Delete = true;
+
+  }
+  openModalUpdate() {
+    
+     this.Update = true;
+
+  }
+  openModalRead() {
+    this.Read = true;
+  }
+
+
+  closeModal() {
+    this.Create = false;
+    this.Read = false;
+    this.Delete = false;
+    this.Update = false;
+  }
 }
