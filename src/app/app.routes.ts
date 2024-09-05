ab4b2f8983b1222b68e124bda79a4cf6efafc4d1
Cristhofer24 <cristhofer.elian.gramal@gmail.com>
2024-09-05 08:19:58 -0500

    c producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'users', component: UsuarioComponent },
+  { path: 'products', component: ProductosComponent },
+
 ];
